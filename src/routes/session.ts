7abf379109191e1e8de89dec7a8e0f08543bf5fc
Agustin Coelho <coelhoagustin1@gmail.com>
2022-07-20 01:40:04 -0300

    mejora logica front y estilos

--- src/routes/session.ts
+++ src/routes/session.ts
@@ -1,14 +1,11 @@
 import express from 'express'
 import passport, {logout} from '../service/session'
 
-const router = express.Router()
+const routerSession = express.Router()
 
-router.get('/user', (req: any, res) => res.send({ data: req.user || null }))
-router.post('/createUser', passport.authenticate('createUser'), (_, res) => res.send({ data: { userLogin: true } }))
-router.post('/validateLogin', passport.authenticate('validateLogin'), (_, res) => res.send({ data: { validate: true } }))
-router.get('/logout', (req, res) => logout(req, res))
-
-
-const routerSession = router
+routerSession.get('/user', (req: any, res) => res.send({ data: req.user || null }))
+routerSession.post('/createUser', passport.authenticate('createUser'), (_, res) => res.send({ data: { userLogin: true } }))
+routerSession.post('/validateLogin', passport.authenticate('validateLogin'), (_, res) => res.send({ data: { validate: true } }))
+routerSession.get('/logout', (req, res) => logout(req, res))
 
 export default routerSession
