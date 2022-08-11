53f540f14944f99c58e717031ec1f0645de195eb
Agustin Coelho <coelhoagustin1@gmail.com>
2022-08-11 02:00:35 -0300

    terminado ngix

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import coockieParser from 'cookie-parser'
 import session from 'express-session'
 import passport from './service/session.js';
 import args from './utils/args.js';
-// import cluster from 'cluster';
-// import os from 'os'
+import cluster from 'cluster';
+import os from 'os'
 
 //Routes
 import routesForItems from './routes/items.js'
@@ -23,7 +23,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static('public')) // se debe comentar por nginx
+// app.use(express.static('public')) // se debe comentar por nginx
 app.use(coockieParser())
 
 app.use(session({
@@ -37,13 +37,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes 
-app.use('/api/sessions', routerSession)
-app.use('/api/items', routesForItems)
-app.use('/api/cart', routesForCart)
-app.use('/api/chat', routerChat)
-// app.use('/', routesForViews)
-app.use('/fork', routerFork)
-app.use('api/randoms', router)
+// app.use('/api/sessions', routerSession)
+// app.use('/api/items', routesForItems)
+// app.use('/api/cart', routesForCart)
+// app.use('/api/chat', routerChat)
+// // app.use('/', routesForViews)
+// app.use('/fork', routerFork)
+app.use('/api/random', router)
 
 const server = http.createServer(app)
 const io = new Server(server);
@@ -59,20 +59,41 @@ io.on('connection', async (socket) => {
 
 const port = args.port || 8080
 
-server.listen(port, () => console.log(`>>> ✅ Server is running in localhost:${port}!`))
-
 // desafio
-// const processId = process.pid
+app.get('/info/', (_, res) => {
+  res.send(`pid: ${process.pid} - numero de procesadores: ${os.cpus().length} - puerto: ${[port]}`)
+})
 
-// app.get('/info', (_, res) => {
-//   res.send(`id: ${processId} - numero de procesadores: ${os.cpus().length}`)
-// })
+if (args.mode === 'cluster' && cluster.isPrimary) {
+  for (let i = 0; i <= os.cpus().length; i++) {
+    cluster.fork()
+  }
 
-// if (process.argv[3] === 'cluster' && cluster.isPrimary) {
-//   for (let i = 0; i <= os.cpus().length; i++) {
-//     cluster.fork()
-//   }
+  cluster.on('exit', (worker, code, signal) => console.log(worker.process.pid, 'died'))
+} else {
+  server.listen(port, () => console.log(`>>> ✅ Server is running in localhost:${port} - worker ${process.pid} started!`))
+}
 
-//   cluster.on('exit', (worker, code, signal) => console.log(worker.process.pid, 'died'))
-// } else {
-// }
+// server.listen(port, () => console.log(`>>> ✅ Server is running in localhost:${port}!`))
+
+/* comandos: 
+* Fork and Cluster
+* nodemon src/index.js --port 9000 --mode fork
+* nodemon src/index.js --port 9000 --mode cluster
+* 
+* Forever
+* forever start src/index.js --port 8080--mode cluster
+* forever start src/index.js --port 8081 --mode cluster
+* forever start src/index.js --port 8082 --mode fork
+*
+* PM2
+* Fork mode: pm2 start src/index.js --name="Server fork" --watch -- 8081
+* Cluster mode: pm2 start src/index.js --name="Server Cluster" -i max --watch -- 8081
+*
+*NGINx: 
+* nodemon src/index.js --port 8080 --mode cluster
+* forever start src/index.js --port 8082 --mode fork
+* forever start src/index.js --port 8083 --mode fork
+* forever start src/index.js --port 8084 --mode fork
+* forever start src/index.js --port 8085 --mode fork
+**/
--- src/utils/args.js
+++ src/utils/args.js
@@ -3,11 +3,14 @@ import yargs from 'yargs';
 const yargsSlice = yargs(process.argv.slice(2))
 
 const args = yargsSlice
-  .default({
-    port: 8070,
-    mode: 'fork'
+  // .default({
+  //   port: 8080,
+  //   mode: 'fork'
+  // })
+  .alias({ 
+    p: 'port', 
+    m: 'mode'
   })
-  .alias({ p: 'port', m: 'mode'})
   .argv
 
 export default args
